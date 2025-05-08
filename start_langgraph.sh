8dd796e7fc054ace402f2f639d635262e48a9f74
felipekreinhandoff <felipe.krein@handoff.ai>
2025-05-08 18:15:51 -0300

    fix script

--- start_langgraph.sh
+++ start_langgraph.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Step 1: Navigate to the project directory
-cd ~/handoff/flatbed || {
+cd ~/handoff/langgraph || {
     echo "Directory ~/handoff/flatbed does not exist."
     exit 1
 }
@@ -10,15 +10,15 @@ cd ~/handoff/flatbed || {
 git pull
 
 # Step 3: Set Python version
-pyenv global 3.10
+pyenv global 3.13
 
 # Step 4: Create virtual environment if it doesn't exist
 if [ ! -d "env" ]; then
-    python3.10 -m venv env
+    python3.13 -m venv env
 fi
 
 # Step 5: Activate virtual environment
 source env/bin/activate
 
 # Step 6: Run the main script
-python3 main.py
+langgraph dev
